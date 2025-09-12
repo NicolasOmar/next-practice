bc6cfba3cec038a219c163edcf20919364400115
NicolasOmar <nicolas.passerino@gmail.com>
2025-09-12 16:52:34 +0200

    fix: How to set page metadata statically and dinamically

--- 6-seo-optimizations/app/page.tsx
+++ 6-seo-optimizations/app/page.tsx
@@ -1,7 +1,31 @@
-'use server'
 import React, { Suspense } from 'react'
+import { Metadata } from 'next'
 import Posts from '@/components/posts'
 import { getPosts } from '@/lib/posts'
+
+/**
+ * metadata property shoud be exported to be included in the head of the document
+ * Besides title and description, you can also add openGraph, twitter and other SEO related properties
+ * Those are just one of many ways to manage SEO in Next.js
+ * In this case, you can set the metadata on a static way by creating an object and exporting it
+ */
+// export const metadata: Metadata = {
+//   title: 'Seo Optimization exercise - Home',
+//   description: "Welcome back! Here's what you might've missed."
+// }
+
+/**
+ * In case you want to generate the metadata dynamically, you have to create a function
+ * in order to fetch the data you want to use in the metadata and return the object
+ */
+export const generateMetadata = async () => {
+  const posts = await getPosts(2)
+
+  return {
+    title: `Seo Optimization exercise - You have ${posts.length} new posts`,
+    description: "Welcome back! Here's what you might've missed."
+  }
+}
 
 const LatestPosts: React.FC = async () => {
   const latestPosts = await getPosts(2)
--- README.md
+++ README.md
@@ -83,6 +83,7 @@ npm start
    - How to cache data from direct data sources with `cache`
 - SEO optimizations (`6-seo-optimizations` folder)
    - How to improve image loading times with `loader` and `priority` properties.
+   - How to set page metadata statically and dinamically
 
 ## Other practice repos
 | Node | Angular | GraphQL | React | HTML & CSS | Styling | Typescript | Docker |
